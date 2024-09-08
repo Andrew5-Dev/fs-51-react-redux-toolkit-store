d25180180ac4339d8bf372d1baf4e7451beeb807
Andrew5-Dev <topchiy.ap@gmail.com>
2024-09-08 17:30:45 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# React App Redux Toolkit
+
+
+Встановлення 
+
+```
+npm install
+```
+
+Режим розробки
+
+```
+npm run dev
+```
+
+Продакшн-версія
+
+```
+yarn build
+```
+
---
+++ src/components/form/SelectField.tsx
@@ -0,0 +1,33 @@
+import { ChangeEvent } from 'react'
+import { ProductCategoriesInterface } from '../../data/mockData.ts'
+
+interface SelectFieldPropsInterface {
+    id: string
+    value: string
+    onChange: (e: ChangeEvent<HTMLSelectElement>) => void
+    options: ProductCategoriesInterface[]
+    required?: boolean
+}
+
+const SelectField = ({ id, value, onChange, options, required = true }: SelectFieldPropsInterface) => {
+    console.log(id)
+    console.log(value)
+
+    return (
+        <div className="form-group">
+            <label className="form-label" htmlFor="category">
+                {id.charAt(0).toUpperCase() + id.slice(1)}:
+            </label>
+            <select className="form-control" id={id} value={value} onChange={onChange} required={required}>
+                <option value="">Please select a {id}...</option>
+                {options.map((option: ProductCategoriesInterface) => (
+                    <option key={option.value} value={option.value}>
+                        {option.text}
+                    </option>
+                ))}
+            </select>
+        </div>
+    )
+}
+
+export default SelectField
